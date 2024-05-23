556f56381a5c5d11d9aa8dd5e6c80f0826ed1efe
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-05-23 09:17:45 -0300

    Adicionando o pacote Serilog e log no console

--- WebApplication_Serilog/Controllers/WeatherForecastController.cs
+++ WebApplication_Serilog/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace WebApplication_Serilog.Controllers
 {
@@ -21,13 +22,19 @@ namespace WebApplication_Serilog.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+
+            Log.Information("Weather Forecast => {@Result}", result);
+
+            return result;
         }
     }
 }
--- WebApplication_Serilog/Program.cs
+++ WebApplication_Serilog/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .CreateLogger();
 
 var app = builder.Build();
 
